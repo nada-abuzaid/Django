c553462c03b218662adf0b3df5cc6744c462c468
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-06-24 12:19:41 +0300

    Try Django

--- playground/urls.py
+++ playground/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name='index'),
+    # path('', views.home_page, name='index'),
     path('home/', views.home_page),
     path('about/', views.about_page),
     path('contact/', views.contact_page),
--- playground/views.py
+++ playground/views.py
@@ -3,11 +3,13 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    # return render(request, 'hello.html', {'name': 'Nada'})
-    return HttpResponse("<h1>Hello Nada!</h1>")
+    my_name = 'Nada!'
+    return render(request, 'hello.html', {'name': my_name})
+
 
 def about_page(request):
     return HttpResponse("<h1>About us!</h1>")
+
 
 def contact_page(request):
     return HttpResponse("<h1>Contact us!</h1>")
